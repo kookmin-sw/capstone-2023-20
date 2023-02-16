7b6177390eed114fb9548939e2309eebf81cea96
KB Kim <jimi567@kookmin.ac.kr>
2023-02-16 15:26:26 +0900

    feat PhotonNetworkUI
    메인타이틀,로비,게임룸,네트워크구축

---
+++ Assets/Scprits/UI/TitleUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class TitleUI : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private GameObject lobbyUI;
+    [SerializeField]
+    private GameObject titleUI;
+    public void OnClickOnlineBtn()
+    {
+        Debug.Log("온라인 버튼 클릭");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+
+
+    public void ClickExitBtn()
+    {
+        Debug.Log("Click Exit");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else 
+        Application.Quit();
+#endif
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("온라인 연결");
+        PhotonNetwork.JoinLobby();
+     
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("로비 참가 완료");
+        titleUI.SetActive(false);
+        lobbyUI.SetActive(true);
+    }
+}

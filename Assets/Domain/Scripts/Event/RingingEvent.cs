9132d9ec96ff0be4890bd1bb6eaac476c9543ae8
rhkrhz <67236054+kwawak@users.noreply.github.com>
2023-03-10 09:30:21 +0900

    add flashlight, bg, bgSound

---
+++ Assets/Domain/Scripts/Event/RingingEvent.cs
@@ -0,0 +1,33 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using Unity.VisualScripting;
+//using UnityEngine;
+
+//public class RingingEvent : MonoBehaviour
+//{
+//    //핸드폰
+//    [SerializeField]
+//    GameObject phone; //핸드폰에서 소리 재생
+
+//    [SerializeField]
+//    Transform tf; //플레이어 위치
+
+//    float dir;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+//// Update is called once per frame
+//    void Update()
+//    {
+//        dir = 
+//    }
+
+//    void OnCollisionEnter(Collision col)
+//    {
+//        if (col.gameObject.name == "handle")
+//            sound.PlayOneShot(sound.clip);
+//    }
+//}

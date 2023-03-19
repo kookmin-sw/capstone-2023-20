a5fc2e1789a4a264bc8424cc9ba52917508cf9a0
rhkrhz <67236054+kwawak@users.noreply.github.com>
2023-03-20 01:59:38 +0900

    maintitle-ing

---
+++ Assets/Domain/Scripts/Sound/MainTitleSound.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainTitleSound : MonoBehaviour
+{
+    // mute랑 피치 왔다갔다하면서 조절
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

81ebd1bbfcd8bc6fd448ea9cb32341090e0018ee
rhkrhz <67236054+kwawak@users.noreply.github.com>
2023-04-07 17:26:02 +0900

    1

--- Assets/Scenes/TrailerScene/AppearObj.cs
+++ Assets/Scenes/TrailerScene/AppearObj.cs
@@ -6,11 +6,21 @@ public class AppearObj : MonoBehaviour
 {
     public GameObject obj;
     public float appearTime = 5f;
+<<<<<<< Updated upstream
+=======
+    public float plusTime = 5f;
+>>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
         obj.SetActive(false);
+<<<<<<< Updated upstream
         Invoke("Appear", appearTime); // n초뒤 함수 호출
+=======
+        Invoke("Appear", appearTime); // n초뒤 함수
+        Invoke("Disappear", appearTime*2); // n초뒤 함수 호출
+        Invoke("Appear", appearTime*2+3);
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
@@ -21,5 +31,14 @@ public class AppearObj : MonoBehaviour
     void Appear()
     {
         obj.SetActive(true);
+<<<<<<< Updated upstream
+=======
+
+    }
+    void Disappear()
+    {
+        obj.SetActive(false);
+
+>>>>>>> Stashed changes
     }
 }

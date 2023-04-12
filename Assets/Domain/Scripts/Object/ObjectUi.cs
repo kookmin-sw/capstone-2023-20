28155ddfbe1431aa438832609c348079b61b7230
Seonghyeon0409 <t139754@gmail.com>
2023-04-13 00:23:21 +0900

    가이드ui 보완

---
+++ Assets/Domain/Scripts/Object/ObjectUi.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectUi : MonoBehaviour
+{
+    public GameObject taichi;
+    public GuideUiManager UiManager;
+    public Timeline[] Timelines;
+    public Timeline Both;
+    public Timeline Main;
+    public Timeline Sub;
+
+    public string maintxt;
+    public string subtxt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Activate()
+    {
+        taichi = GameObject.FindGameObjectWithTag("Taichi");
+        UiManager = taichi.GetComponentInChildren<GuideUiManager>();
+        Timelines = taichi.GetComponentsInChildren<Timeline>();
+        if (Timelines.Length > 0)
+        {
+            Debug.Log(Timelines[1]);
+            // 원하는 스크립트를 선택하여 사용
+            Both = Timelines[0];
+            Main = Timelines[1];
+            Sub = Timelines[2];
+        }
+
+        //Transform MainContoller = taichi.transform.Find("MainContloller");
+        //Transform SubContoller = taichi.transform.Find("SubContloller");
+        //MainTimeline = MainContoller.GetComponent<Timeline>();
+        //SubTimeline = SubContoller.GetComponent<Timeline>();
+    }
+    //public void SetMainPanelText(string txt)
+    //{
+    //    maintxt = txt;
+    //}
+    //public void SetSubPanelText(string txt)
+    //{
+    //    subtxt = txt;
+    //}
+    public void SetGuideUi()
+    {
+        UiManager.SetMainPanelText(maintxt);
+        UiManager.SetSubPanelText(subtxt);
+
+    }
+    public void TimeLineActive()
+    {
+        Both.Play();
+    }
+}

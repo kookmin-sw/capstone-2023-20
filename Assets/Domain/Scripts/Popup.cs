385251133f8e8cee95234c7b869fc865b87fc2bf
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-04-14 17:55:05 +0900

    bug fix

--- Assets/Domain/Scripts/Popup.cs
+++ Assets/Domain/Scripts/Popup.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class Popup : MonoBehaviour
 {
     public GameObject popup;
-    public static Popup instance { get; private set; }
 
     private void Awake()
-    {
-        if (null == instance)
-            instance = this;
+    { 
 
     }
 
--- Assets/Domain/Scripts/ThirdPlayerController.cs
+++ Assets/Domain/Scripts/ThirdPlayerController.cs
@@ -51,16 +51,13 @@ public class ThirdPlayerController : MonoBehaviour
     float hitDistance = 2f;
     public bool InvestigateValue = false;
 
-    Material outline;
-
-    Renderer renderers;
-    List<Material> materialList = new List<Material>();
-
     Vector3 mouseWorldPosition = Vector3.zero;
+
+    // 팝업창
+    private Popup popup;
 
     private void Start()
     {
-        outline = new Material(Shader.Find("Custom/OutLine"));
     }
 
     private void Awake()
@@ -69,6 +66,7 @@ public class ThirdPlayerController : MonoBehaviour
         playerInputs = GetComponent<StarterAssetsInputs>();
         thirdPersonController = GetComponent<ThirdPersonController>();
         animator = GetComponent<Animator>();
+        popup = GetComponentInChildren<Popup>();
     }
     private void Update()
     {
@@ -90,10 +88,8 @@ public class ThirdPlayerController : MonoBehaviour
                 if (hit.collider.CompareTag("PuzzleObj"))
                 {
 
-                    // 상호작용 버튼 활성화
-                    Popup.instance.OpenPopUp();
-
-                    // 상호작용 메세지 활성화
+                    // 상호작용 메시지 활성화
+                    popup.OpenPopUp();
                     if (playerInputs.investigate == true)
                     {
                         InvestigateValue = true;
@@ -104,7 +100,7 @@ public class ThirdPlayerController : MonoBehaviour
                 }
                 else if (hit.collider.CompareTag("EventObj"))
                 {
-                    Popup.instance.OpenPopUp();
+                    popup.OpenPopUp();
                     // 김원진 - 잠긴 문인지 확인
                     if (playerInputs.investigate == true)
                     {
@@ -120,7 +116,7 @@ public class ThirdPlayerController : MonoBehaviour
                 }
                 else if (hit.collider.CompareTag("LockerUnlocked"))
                 {
-                    Popup.instance.OpenPopUp();
+                    popup.OpenPopUp();
                     if (playerInputs.investigate == true)
                     {
                         // 유성현 - UnityEvent Invoke를 이용해 서로 다른 함수를 호출 할 수 있도록 확장
@@ -147,13 +143,13 @@ public class ThirdPlayerController : MonoBehaviour
 
                 else
                 {
-                    Popup.instance.ClosePopUp();
+                    popup.ClosePopUp();
                 }
             }
             // raycast에 물체가 없을 시
             else
             {
-                Popup.instance.ClosePopUp();
+                popup.ClosePopUp();
             }
             if (isCollision == false)
             {

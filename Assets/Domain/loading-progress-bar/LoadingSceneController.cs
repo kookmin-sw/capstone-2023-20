c624d1ac141abcc10fdc1fd48dfbacad2b5d01cf
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-04-20 04:00:42 +0900

    fix1
    씬 로딩 방식을 유니티 씬 매니저에서
    포톤 loadlevel로 변경.
    connectionHandler의 KeepAliveInBackground값을 200000로 증가

--- Assets/Domain/loading-progress-bar/LoadingSceneController.cs
+++ Assets/Domain/loading-progress-bar/LoadingSceneController.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEngine.SceneManagement;
-using static Cinemachine.DocumentationSortingAttribute;
+using Photon.Pun;
+using Photon.Realtime;
+
 
 public class LoadingSceneController : MonoBehaviour
 {
@@ -15,46 +16,43 @@ public class LoadingSceneController : MonoBehaviour
 
     static int nextLevel;
 
-    
+    static string[] levels = { "MainTitle", "MainBuilding", "Stage2", "Stage3" };
     static string[] tips = { "tip1", "tip2", "tip3", "tip4", "tip5" };
     public static void LoadScene(int level)
     {
         nextLevel = level;
-        SceneManager.LoadScene("LoadingScene");
+        PhotonNetwork.LoadLevel("LoadingScene");
 
     }
 
     void Start()
     {
-        StartCoroutine(LoadSceneProcess());
         tip.text = "Tips : " + tips[nextLevel + 1];
+        
+        if (PhotonNetwork.IsMasterClient)
+        {
+            LoadStage();
+        }
     }
 
-    IEnumerator LoadSceneProcess()
+    void LoadStage()
     {
-        AsyncOperation op = SceneManager.LoadSceneAsync(nextLevel);
-        op.allowSceneActivation = false; //로딩 시간이 너무 짧은걸 방지(아마 스쿨씬이 커서 필요없을 수도 있음
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            return;
+        }
+        PhotonNetwork.LoadLevel(levels[nextLevel]);
+    }
 
-        float timer = 0f;
+    /*IEnumerator LoadSceneProcess()
+    {
+        AsyncOperation op;
+        //op.allowSceneActivation = false; //로딩 시간이 너무 짧은걸 방지(아마 스쿨씬이 커서 필요없을 수도 있음
         while (!op.isDone)
         {
+            progressBar.fillAmount = op.progress;
             yield return null;
-
-            if (op.progress < 0.9f)
-            {
-                progressBar.fillAmount = op.progress;
-            }
-            else
-            {
-                timer = Time.unscaledDeltaTime;
-                progressBar.fillAmount = Mathf.Lerp(0.9f, 1f, timer);
-                if(progressBar.fillAmount >= 1f)
-                {
-                    op.allowSceneActivation = true;
-                    yield break;
-                }
-            }
         }
-
-    }
+    }*/
 }

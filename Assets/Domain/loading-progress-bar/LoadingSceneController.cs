382a85f3308b3562aefe8bf05f0eb0c55550affa
rhkrhz <67236054+kwawak@users.noreply.github.com>
2023-05-23 15:53:58 +0900

    1

--- Assets/Domain/loading-progress-bar/LoadingSceneController.cs
+++ Assets/Domain/loading-progress-bar/LoadingSceneController.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using TMPro;
 using Photon.Pun;
 using Photon.Realtime;
-using UnityEngine.UIElements;
+//using UnityEngine.UIElements;
 
 
 public class LoadingSceneController : MonoBehaviourPunCallbacks
